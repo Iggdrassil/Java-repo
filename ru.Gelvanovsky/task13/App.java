3ee81bb460a19d6c415597dd5a31ee79b67e1db3
Iggdrassil <Cidorov1124@yandex.ru>
2019-06-03 19:07:54 +0600

    HomeWork homeLibraryCorrected

--- ru.Gelvanovsky/task13/App.java
+++ ru.Gelvanovsky/task13/App.java
@@ -1,25 +1,24 @@
 package ru.Gelvanovsky.task13;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Scanner;
 
 public class App implements Serializable {
     private static final long serialVersionUID = 2L;
-    //private static final String fileName = "HomeLibrary.bin";
     static Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Library library = new Library();
-        System.out.println("Выберите действие:");
-        System.out.println("[Д]обавить книги");
-        System.out.println("[П]оказать список существующих книг");
+        System.out.println("[Д]обавить книгу");
+        System.out.println("[П]росмотреть существующие книги");
         String userChoise = in.nextLine();
         userChoise = userChoise.toUpperCase();
         userChoise = userChoise.trim();
         if (userChoise.equals("Д")) {
             library.addBook();
-            System.out.println("Книги добавлены");
+            System.out.println("Книга добавлена");
         }
         if (userChoise.equals("П")){
-            library.viewBooksInLibray();}
+            library.readLibrary();}
     }
 }
--- ru.Gelvanovsky/task13/Library.java
+++ ru.Gelvanovsky/task13/Library.java
@@ -5,39 +5,59 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library implements Serializable {
+    static Scanner in = new Scanner(System.in);
     private static final long serialVersionUID = 3L;
     private static final File fileName = new File("HomeLibrary.bin");
-    void addBook() {
-        ArrayList<Book>books = new ArrayList<>();
-        books.add(new Book("Книга1","Автор1","1990"));
-        books.add(new Book("Книга2","Автор2","1992"));
-        books.add(new Book("Книга3","Автор3","1993"));
-        books.add(new Book("Книга4","Автор4","1994"));
-        books.add(new Book("Книга5","Автор5","1995"));
-        books.add(new Book("Книга6","Автор6","1996"));
-        books.add(new Book("Книга7","Автор7","1997"));
-        try (FileOutputStream addingBook = new FileOutputStream(fileName);
-             ObjectOutputStream oos = new ObjectOutputStream(addingBook)) {
-              oos.writeObject(books);
-              oos.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-                        e.printStackTrace();
-        }
-    }
-    void viewBooksInLibray(){
+    static ArrayList<Book> newBooks = new ArrayList<>();
+    static ArrayList<Object> oldBooks = new ArrayList<>();
+    void readLibrary(){
         try (FileInputStream listBooks = new FileInputStream(fileName);
              ObjectInputStream ois = new ObjectInputStream(listBooks)) {
-            //System.out.println(ois.readUTF());
-            System.out.println(ois.readObject());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println(ois.readObject());}
+        catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
+    }
+    void saveBook(ArrayList<Object> books){
+        try (FileOutputStream addingBook = new FileOutputStream(fileName);
+             ObjectOutputStream oos = new ObjectOutputStream(addingBook)) {
+            oos.writeObject(oldBooks);
+            oos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+    }}
+     void addBook() throws IOException, ClassNotFoundException {
+        System.out.println("Введи название:");
+        String nameOfBook = in.nextLine();
+        System.out.println("Введи автора:");
+        String author = in.nextLine();
+        System.out.println("Введи год:");
+        String date = in.nextLine();
+        if ((fileName.length()==0)==true){
+            //выполнение этого условия означает, что файл пуст
+            System.out.println("File is empty");
+            Book bookNewWhenFileIsEmpty = new Book(nameOfBook,author,date);
+            newBooks.add(bookNewWhenFileIsEmpty);//добавляем новую книгу
+            oldBooks.addAll(0,newBooks);//добавляем новую книгу в массив старых
+            saveBook(oldBooks);//сохраняем
+        }else {
+        FileInputStream book = new FileInputStream(fileName);
+        ObjectInputStream ois = new ObjectInputStream(book);
+        Object oldData = String.valueOf(ois.readObject());
+        //System.out.println(oldData);
+        //System.out.println("Файл прочитан!");
+        oldBooks.add(oldData);
+        ois.close();
+        //System.out.println("Файл прочитан!x2");
+        //newBooks.addAll(0,oldBooks);//добавляем все старые книги
+        Book bookNew = new Book(nameOfBook,author,date);
+        oldBooks.add(bookNew);
+        saveBook(oldBooks);
+        }
     }
 }
+
